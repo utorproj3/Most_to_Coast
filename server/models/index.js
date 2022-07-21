d40cf76ea05b1ec9e4fc748175f9fb1af78b7b6f
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-07-21 17:40:20 -0400

    add index.js to models

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,7 @@
 const User = require('./User');
+const Activity = require('./Activity');
+const BasicPlanInfo = require('./BasicPlanInfo');
+const Location = require('./Location');
+const TravelPlan = require('./TravelPlan');
 
-module.exports = { User };
+module.exports = { User, Activity, BasicPlanInfo, Location, TravelPlan };
