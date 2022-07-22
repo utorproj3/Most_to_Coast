79a3acd752c04bcc70a1b720c18d0e61dc5d3355
Tyler Flynn <tyler94flynn@gmail.com>
2022-07-21 20:10:11 -0400

    troubleshooting routes

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// import Auth from '../../utils/auth';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>test</h1>
+    </header>
+  );
+};
+
+export default Header;
