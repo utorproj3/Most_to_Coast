d52d74ecb658f68cd944624fa30b545c8854d7dc
Yurippe <yurichikawa1992@gmail.com>
2022-07-22 13:30:49 -0400

    add queries to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,36 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
+const { User, Plan, Day, Activity } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    
+    me: async (parent, args, context) => {
+      // if there is user token in req headers, return user data, or else throw err
+      if (context.user) {
+        return User.findOne({ _id: context.user._id })
+          .populate('myPlans');
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+    allUsers: async (parent, args, context) => {
+      return User.find();
+    },
+    allTravelPlans: async () => {
+      if (context.user) {
+        return Plan.find();
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+    singlePlan: async (parent, { _id }) => {
+      if (context.user) {
+        return Plan.findOne({ _id })
+          .populate('days');
+      }
+
+      throw new AuthenticationError('Not logged in');
+    }
   },
 
   Mutation: {
