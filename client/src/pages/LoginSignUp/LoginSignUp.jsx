8c503a46a3b36af88450e3e475ec0c3fd23589d4
Mikel Balazic <mikelbalazic@Mikels-iMac.local>
2022-07-22 17:59:28 -0400

    LoginSignUp Elements

--- client/src/pages/LoginSignUp/LoginSignUp.jsx
+++ client/src/pages/LoginSignUp/LoginSignUp.jsx
@@ -1,10 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginSignUp.css";
 
 export default function LoginSignUp() {
     return (
-        <div>
-            signup test
-        </div>
+        <body>
+            <div className='full-form'>
+                {/* <ul className='tab-group'>
+                    <li className='tab-active'><a href="#signup"></a>Sign Up</li>
+                    <li className='tab'><a href="#login">Log In</a></li>
+                </ul> */}
+
+                <div className='tab-content'>
+                    <div id="signup">
+
+                        <h1>Sign Up</h1>
+
+                        <form action="/" method="post">
+
+                            <div className='top-row'>
+                                <div className='field-wrap'>
+                                    <label>
+                                        First Name<span className='req'></span>
+                                    </label>
+                                    <input type='text' required autocomplete="off" />
+                                </div>
+                                <div className='field-wrap'>
+                                    <label>
+                                        Last Name<span className='req'></span>
+                                    </label>
+                                    <input type="text" required autocomplete="off" />
+                                </div>
+                            </div>
+
+                            <div className='field-wrap'>
+                                <label>
+                                    Email Address<span className='req'></span>
+                                </label>
+                                <input type="email" required autocomplete="off" />
+                            </div>
+
+                            <div className='field-wrap'>
+                                <label>
+                                    Set A Password<span className='req'></span>
+                                </label>
+                                <input type="password" required autocomplete="off" />
+                            </div>
+
+                            <button type="submit" className='button'>Get Started</button>
+
+                        </form>
+                    </div>
+
+                    <div id='login'>
+                        <h1>Welcome Back</h1>
+
+                        <form action="/" method="post">
+
+                            <div className='field-wrap'>
+                                <label>
+                                    Email Address<span className='req'></span>
+                                </label>
+                                <input type="email" required autocomplete="off" />
+                            </div>
+
+                            <div className='field-wrap'>
+                                <label>
+                                    Password<span className='req'></span>
+                                </label>
+                                <input type="password" required autocomplete="off" />
+                            </div>
+
+                            <button className='button'>Log In</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </body>
     )
 }
