42d896f11041ba7c91365c1a05e0ff6c06bc0cb2
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-07-22 19:50:56 -0400

    feature/Account

--- client/src/pages/Account/Account.jsx
+++ client/src/pages/Account/Account.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "./Account.css";
+import Auth from "../../utils/auth";
+import LoginSignUp from "../LoginSignUp/LoginSignUp";
+const About = () => {
+  const loggedIn = Auth.loggedIn();
 
-export default function Account() {
-    return (
-        <div>
-            Account test
-        </div>
-    )
-}
+  return (
+    // if NOT logged in, login/signup component, else (logged in), here's the Account Page
+    <>
+      {!loggedIn ? (
+        <LoginSignUp />
+      ) : (
+        <div>Thanks for logging in, Enter a travel plan</div>
+      )}
+    </>
+  );
+};
+
+export default About;
