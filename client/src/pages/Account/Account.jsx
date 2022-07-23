105ccf227336df543dbd30a61346dfce19679690
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-07-22 21:47:40 -0400

    added testing avatar pic, finished form

--- client/src/pages/Account/Account.jsx
+++ client/src/pages/Account/Account.jsx
@@ -2,22 +2,30 @@ import React, { useState } from "react";
 import "./Account.css";
 import Auth from "../../utils/auth";
 import LoginSignUp from "../LoginSignUp/LoginSignUp";
+import AvatarHolder from "../../assets/img/Profile-holder.jpg";
 const About = () => {
   const loggedIn = Auth.loggedIn();
   const [userName, setUserName] = useState("");
   const [aboutMe, setAboutMe] = useState("");
+  const [avatarPic, setAvatarPic] = useState({ AvatarHolder });
 
   const handleNameChange = (e) => {
     var userInput = e.target.value.trim();
     if (userInput.length !== 0) {
       setUserName(userInput);
     }
+    console.log("Please enter a username:!");
   };
   const handleDescriptionChange = (e) => {
     var descpInput = e.target.value.trim();
     if (descpInput.length !== 0) {
       setAboutMe(descpInput);
     }
+    console.log("Please enter a description");
+  };
+  const handleAvatarChange = (e) => {
+    var imgInput = e.target.value;
+    setAvatarPic(imgInput);
   };
 
   return (
@@ -32,24 +40,43 @@ const About = () => {
               <label>
                 Username:
                 <input
-                  placeholder="username"
-                  required
+                  value={userName}
                   onChange={handleNameChange}
+                  placeholder="username"
+                  id="username"
+                  required
                 />
                 Username
               </label>
+            </div>
+            <div>
               <label>
                 About Me:
                 <textarea
-                  placeholder="About me.."
-                  required
+                  value={aboutMe}
                   onChange={handleDescriptionChange}
+                  placeholder="About me.."
+                  id="about-me"
+                  required
                 >
                   Hobbies, Activities, Personality..
                 </textarea>
               </label>
             </div>
-            <button>Submit</button>
+            <div>
+              <img src={avatarPic} alt="avatarPicture" />
+              <label>
+                Choose an Avatar:
+                <input
+                  type="file"
+                  id="avatar"
+                  accept="image/png, image/jpeg"
+                  onChange={handleAvatarChange}
+                />
+              </label>
+            </div>
+
+            <button type="submit">Submit</button>
           </form>
         </div>
       )}
