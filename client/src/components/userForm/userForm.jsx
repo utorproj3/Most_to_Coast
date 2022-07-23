55d72c56910fa87c6816cf0a182ae71fa11ccbe9
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-07-22 21:59:13 -0400

    feature/Account-userForm

---
+++ client/src/components/userForm/userForm.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import AvatarHolder from "../../assets/img/Profile-holder.jpg";
+
+const UserForm = () => {
+  const [userName, setUserName] = useState("");
+  const [aboutMe, setAboutMe] = useState("");
+  const [avatarPic, setAvatarPic] = useState({ AvatarHolder });
+
+  const handleNameChange = (e) => {
+    var userInput = e.target.value.trim();
+    if (userInput.length !== 0) {
+      setUserName(userInput);
+    }
+    console.log("Please enter a username:!");
+  };
+  const handleDescriptionChange = (e) => {
+    var descpInput = e.target.value.trim();
+    if (descpInput.length !== 0) {
+      setAboutMe(descpInput);
+    }
+    console.log("Please enter a description");
+  };
+  const handleAvatarChange = (e) => {
+    var imgInput = e.target.value;
+    setAvatarPic(imgInput);
+  };
+
+  return (
+    <div>
+      <form>
+        <div className="field-wrap">
+          <label>
+            Username:
+            <input
+              value={userName}
+              onChange={handleNameChange}
+              placeholder="username"
+              id="username"
+              required
+            />
+            Username
+          </label>
+        </div>
+        <div>
+          <label>
+            About Me:
+            <textarea
+              value={aboutMe}
+              onChange={handleDescriptionChange}
+              placeholder="About me.."
+              id="about-me"
+              required
+            >
+              Hobbies, Activities, Personality..
+            </textarea>
+          </label>
+        </div>
+        <div>
+          <img src={avatarPic} alt="avatarPicture" />
+          <label>
+            Choose an Avatar:
+            <input
+              type="file"
+              id="avatar"
+              accept="image/png, image/jpeg"
+              onChange={handleAvatarChange}
+            />
+          </label>
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default UserForm;
--- client/src/pages/Account/Account.jsx
+++ client/src/pages/Account/Account.jsx
@@ -1,84 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Account.css";
 import Auth from "../../utils/auth";
 import LoginSignUp from "../LoginSignUp/LoginSignUp";
-import AvatarHolder from "../../assets/img/Profile-holder.jpg";
+import UserForm from "../../components/userForm/userForm";
 const About = () => {
   const loggedIn = Auth.loggedIn();
-  const [userName, setUserName] = useState("");
-  const [aboutMe, setAboutMe] = useState("");
-  const [avatarPic, setAvatarPic] = useState({ AvatarHolder });
-
-  const handleNameChange = (e) => {
-    var userInput = e.target.value.trim();
-    if (userInput.length !== 0) {
-      setUserName(userInput);
-    }
-    console.log("Please enter a username:!");
-  };
-  const handleDescriptionChange = (e) => {
-    var descpInput = e.target.value.trim();
-    if (descpInput.length !== 0) {
-      setAboutMe(descpInput);
-    }
-    console.log("Please enter a description");
-  };
-  const handleAvatarChange = (e) => {
-    var imgInput = e.target.value;
-    setAvatarPic(imgInput);
-  };
 
   return (
     // if NOT logged in, login/signup component, else (logged in), here's the Account Page
+    // UserForm used to state/change username, description about user, and icon/avatar pic
     <>
       {!loggedIn ? (
         <LoginSignUp />
       ) : (
-        <div>
-          <form>
-            <div className="field-wrap">
-              <label>
-                Username:
-                <input
-                  value={userName}
-                  onChange={handleNameChange}
-                  placeholder="username"
-                  id="username"
-                  required
-                />
-                Username
-              </label>
-            </div>
-            <div>
-              <label>
-                About Me:
-                <textarea
-                  value={aboutMe}
-                  onChange={handleDescriptionChange}
-                  placeholder="About me.."
-                  id="about-me"
-                  required
-                >
-                  Hobbies, Activities, Personality..
-                </textarea>
-              </label>
-            </div>
-            <div>
-              <img src={avatarPic} alt="avatarPicture" />
-              <label>
-                Choose an Avatar:
-                <input
-                  type="file"
-                  id="avatar"
-                  accept="image/png, image/jpeg"
-                  onChange={handleAvatarChange}
-                />
-              </label>
-            </div>
-
-            <button type="submit">Submit</button>
-          </form>
-        </div>
+        <di>
+          <UserForm />
+          <div></div>
+        </di>
       )}
     </>
   );
