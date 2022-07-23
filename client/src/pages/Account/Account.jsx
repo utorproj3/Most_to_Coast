4f71f76ff8e32c81a64adcb337f740f13f591bf9
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-07-22 20:58:25 -0400

    added handleUserChange for username edit in Account page

--- client/src/pages/Account/Account.jsx
+++ client/src/pages/Account/Account.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Account.css";
 import Auth from "../../utils/auth";
 import LoginSignUp from "../LoginSignUp/LoginSignUp";
 const About = () => {
   const loggedIn = Auth.loggedIn();
+  const [userName, setUserName] = useState("");
+  const [aboutMe, setAboutMe] = useState("");
+
+  const handleNameChange = (e) => {
+    var userInput = e.target.value;
+    if (userInput !== "") {
+      setUserName(userInput);
+    }
+  };
 
   return (
     // if NOT logged in, login/signup component, else (logged in), here's the Account Page
@@ -11,7 +20,28 @@ const About = () => {
       {!loggedIn ? (
         <LoginSignUp />
       ) : (
-        <div>Thanks for logging in, Enter a travel plan</div>
+        <div>
+          <form>
+            <div className="field-wrap">
+              <label>
+                Username:
+                <input
+                  placeholder="username"
+                  required
+                  onChange={handleNameChange}
+                />
+                Username
+              </label>
+              <label>
+                About Me:
+                <textarea placeholder="About me.." required>
+                  Hobbies, Activities, Personality..
+                </textarea>
+              </label>
+            </div>
+            <button>Submit</button>
+          </form>
+        </div>
       )}
     </>
   );
