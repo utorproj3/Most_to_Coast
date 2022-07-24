fdb31a757280815fc94e35caa78a4132aa93be32
Yurippe <yurichikawa1992@gmail.com>
2022-07-24 15:38:45 -0400

    debug the seed data returning errs and incorrect dayNumbers

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -1,4 +1,4 @@
-const faker = require('faker');
+const { faker } = require('@faker-js/faker');
 
 const db = require('../config/connection');
 const { Activity, Day, Plan, User } = require('../models');
@@ -25,60 +25,66 @@ db.once('open', async () => {
   const createdUsers = await User.collection.insertMany(userData);
 
   // create plans
-  let createdPlans = [];
+  let createdDays = [];
   for (let i = 0; i < 20; i += 1) {
     // choose user at random
-    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
-    const { _id: userId } = createdUsers.ops[randomUserIndex];
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.insertedCount);
+    const { _id: userId } = Object.entries(createdUsers.insertedIds)[randomUserIndex][1];
 
     // create plan data
     const planTitle = faker.lorem.words(Math.round(Math.random() * 5) + 1);
-    const destination = faker.address.cityName();
-    const descriptionText = faker.lorem.sentences();
+    const destination = faker.address.city();
+    const descriptionText = faker.lorem.sentences(2, 3);
     const startDate = faker.date.future();
-    const endDate = faker.date.between(startDate, startDate + (Math.round(Math.random() * 10) + 1));
-
+    const endDate = new Date().setDate(startDate.getDate() + (Math.round(Math.random() * 10) + 1));
+    
     // create plan in db
-    const createdPlan = await Plan.create({ planTitle, destination, descriptionText, startDate, endDate });
+    const plan = await Plan.create({ planTitle, destination, descriptionText, startDate, endDate });
     await User.findOneAndUpdate(
       { _id: userId },
-      { $push: { myPlans: createdPlan._id } },
+      { $push: { myPlans: plan._id } },
       { new: true },
     );
 
     // create 1st day in the plan
     const createdDay = await Day.create({ dayNumber: 1 });
     await Plan.findOneAndUpdate(
-      { _id: createdPlan._id },
-      { $addToSet: { days: createdDay } },
+      { _id: plan._id },
+      { $push: { days: createdDay } },
       { new: true }
     );
 
-    createdPlans.push(createdPlan);
+    createdDays.push(createdDay);
   }
 
+  
   // create Days
-  let createdDays = [];
-  for (let i = 0; i < 60; i += 1) {
+  for (let i = 0; i < 30; i += 1) {
     // randomly select plan
-    const randomPlanIndex = Math.floor(Math.random() * createdPlans.ops.length);
-    const { _id: planId, dayNumber } = createdPlans.ops[randomPlanIndex];
+    const randomPlans = await Plan.find().populate('days'); // <- This is to renew the info to increment dayNumber
+    const randomPlanIndex = Math.floor(Math.random() * randomPlans.length);
+    const selectedPlan = randomPlans[randomPlanIndex];
+
+    // get the last day's dayNumber
+    const existDays = selectedPlan.days.length;
+    let { dayNumber } = selectedPlan.days[existDays - 1];
+
     // increment the number from randomly selected Plan's dayNumber
-    const dayNumberData =+ dayNumber;
+    ++dayNumber;
 
     // create Day in db
-    const createdDay = await Day.create({ dayNumber: dayNumberData });
+    const createdDay = await Day.create({ dayNumber: dayNumber });
     await Plan.findOneAndUpdate(
-      { _id: planId },
-      { $addToSet: { days: createdDay } },
+      { _id: selectedPlan._id },
+      { $push: { days: createdDay } },
       { new: true }
     );
 
-   createdDays.push(createdDay); 
+    createdDays.push(createdDay); 
   }
-
+  
   // create activities
-  for (let i = 0; i < 100; i += 1) {
+  for (let i = 0; i < 60; i += 1) {
     // create activity data
     const name = faker.lorem.words(Math.round(Math.random() * 3) + 1);
     const place = [
@@ -91,30 +97,35 @@ db.once('open', async () => {
     let startHour;
     let amOrPm;
     if (faker.datatype.boolean()) {
-      startHour = Math.floor(Math.random() * (12 + 1 - 9)) + 9;
+      // 9:00am to 11:59am
+      startHour = Math.floor(Math.random() * (11 + 1 - 9)) + 9;
       amOrPm = 'am';
     } else {
+      // 12:00pm to 11:59pm
       startHour = Math.floor(Math.random() * 12) + 1;
       amOrPm = 'pm';
     }
+
+    // add leading 0 to 1 digit number
+    const minute = String(Math.floor(Math.random() * 60)).padStart(2, '0');
     
     const startTime = [
       startHour, 
       ':', 
-      Math.floor(Math.random() * 60),
+      minute,
       amOrPm
     ].join('');
     // add 1 hour to the startTime as endTime
     const endTime = [
       startHour + 1,
       ':', 
-      Math.floor(Math.random() * 60),
+      minute,
       amOrPm
     ].join('');
     
     // randomly select day
-    const randomDayIndex = Math.floor(Math.random() * createdDays.ops.length);
-    const { _id: dayId } = createdDays.ops[randomDayIndex];
+    const randomDayIndex = Math.floor(Math.random() * createdDays.length);
+    let { _id: dayId } = createdDays[randomDayIndex];
 
     // create activity in db
     const createdActivity = await Activity.create({ name, place, startTime, endTime });
