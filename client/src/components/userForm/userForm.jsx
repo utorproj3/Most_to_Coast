429ab6f31c59ab4a761b91c4a87ad102d9602b60
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-07-25 18:54:30 -0400

    added userForm and Account page, some responsiveness

--- client/src/components/userForm/userForm.jsx
+++ client/src/components/userForm/userForm.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import AvatarHolder from "../../assets/img/Profile-holder.jpg";
+import "./userForm.css";
 
 const UserForm = () => {
   const [userName, setUserName] = useState("");
   const [aboutMe, setAboutMe] = useState("");
-  const [avatarPic, setAvatarPic] = useState({ AvatarHolder });
+  const [avatarPic, setAvatarPic] = useState(AvatarHolder);
 
   const handleNameChange = (e) => {
     var userInput = e.target.value.trim();
@@ -29,46 +30,53 @@ const UserForm = () => {
   return (
     <div>
       <form>
-        <div className="field-wrap">
-          <label>
-            Username:
-            <input
-              value={userName}
-              onChange={handleNameChange}
-              placeholder="username"
-              id="username"
-              required
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            About Me:
-            <textarea
-              value={aboutMe}
-              onChange={handleDescriptionChange}
-              placeholder="About me.."
-              id="about-me"
-              required
-            >
-              Hobbies, Activities, Personality..
-            </textarea>
-          </label>
-        </div>
-        <div>
-          <img src={avatarPic} alt="avatarPicture" />
-          <label>
-            Choose an Avatar:
-            <input
-              type="file"
-              id="avatar"
-              accept="image/png, image/jpeg"
-              onChange={handleAvatarChange}
-            />
-          </label>
+        <div className="form-container">
+          <div className="avatar-container">
+            <img src={avatarPic} alt="avatarPicture" id="avatarPicture" />
+            <label>
+              Choose an Avatar:
+              <input
+                type="file"
+                id="avatar"
+                accept="image/png, image/jpeg"
+                onChange={handleAvatarChange}
+              />
+            </label>
+          </div>
+          <div className="user-form">
+            <div>
+              <label>
+                Username:
+                <input
+                  value={userName}
+                  onChange={handleNameChange}
+                  placeholder="Username"
+                  id="username"
+                  required
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                About Me:
+                <textarea
+                  value={aboutMe}
+                  onChange={handleDescriptionChange}
+                  placeholder="About me.."
+                  id="about-me"
+                  rows={5}
+                  required
+                >
+                  Hobbies, Activities, Personality..
+                </textarea>
+              </label>
+            </div>
+          </div>
         </div>
 
-        <button type="submit">Submit</button>
+        <button type="submit" className="submit-btn">
+          Submit
+        </button>
       </form>
     </div>
   );
