0c833258e6fda85b010bb6ecc7d91a4a5945c769
Mikel Balazic <mikelbalazic@Mikels-iMac.local>
2022-07-25 16:05:05 -0400

    Search Button and Clouds

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -5,6 +5,9 @@ export default function Main() {
     return (
         <div className='main'>
 
+            <img src="img/cloud1.png" className="cloud1" alt="cloud1" />
+            <img src="img/cloud2.png" className="cloud2" alt="cloud2" />
+
             <form className='search-form'>
                 <div className='search-wrap'>
                     <label className='search-input-text'>
