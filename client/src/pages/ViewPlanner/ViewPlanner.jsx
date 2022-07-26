f46678949532ca5673e30043c67bc1f3628c90f8
yomi odusi <yomiodusi98@gmail.com>
2022-07-26 14:29:20 -0400

    finished viewplan template

--- client/src/pages/ViewPlanner/ViewPlanner.jsx
+++ client/src/pages/ViewPlanner/ViewPlanner.jsx
@@ -3,88 +3,128 @@ import "./ViewPlanner.css";
 
 export default function ViewPlanner() {
     return (
-        <div className='viewplan'>
+        <div className="viewplan" >
+            <div class="row">
+                <div class="col-12 col-md-8">
 
-            <div class='parent'>
-                <div class='plan-title'>Plan Title</div>
+                    <div class='parent'>
+                        <div className="plan-title">Plan Title</div>
 
-                <div class='plan-nickname'>Plan Nickname</div>
+                        <div className="plan-nickname">Plan Nickname</div>
+                    </div>
+
+                </div>
+                
+                <div class="col-6 col-md-4">
+                    <div className="plan-details">Plan Details</div>
+                </div>
+            </div>
+        
+
+            <div class="row">
+                <div class="col-6 col-md-2">
+                    <div class="vstack gap-3">
+                        <br></br>
+                        <div className="user-plans">First Plan</div>
+                        <br></br>
+                        <div class="user-plans">Second PLan</div>
+                        <br></br>
+                        <div class="user-plans">Third plan</div>
+                        <br></br>
+                        <div class="user-plans">Fourth plan</div>
+                        <br></br>
+                        <div class="user-plans">Fifth plan</div>
+                        <br></br>
+                        <div class="user-plans">Sixth plan</div>
+                        <br></br>
+                        
+                    </div>
+                </div>
+
+                <div class="col-6 col-md-3">
+
+                <br></br>
+
+                    <textarea id="story" name="story" rows="11" cols="45">
+                                    Description...
+                    </textarea>
+                </div>
+
+                <br></br>
+
+                <div class="col-6 col-md-7">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Day 1</th>
+                            <th scope="col">Day 2</th>
+                            <th scope="col">Day 3</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <th scope="row">9 am</th>
+                            <td>Activity</td>
+                            <td>Activity</td>
+                            <td>Activity</td>
+                            </tr>
+                            <tr>
+                            <th scope="row">12 pm</th>
+                            <td>Activity</td>
+                            <td>Activity</td>
+                            <td>Activity</td>
+                            </tr>
+                            <tr>
+                            <th scope="row">3 pm</th>
+                            <td>Activityy</td>
+                            <td>Activity</td>
+                            <td>Activity</td>
+                            </tr>
+                            <tr>
+                            <th scope="row">7 pm</th>
+                            <td>Activity</td>
+                            <td>Activity</td>
+                            <td>Activity</td>
+                            </tr>
+                            <tr>
+                            <th scope="row">11 pm</th>
+                            <td>Activity</td>
+                            <td>Activity</td>
+                            <td>Activity</td>
+                            </tr>
+                            
+                        </tbody>
+                    </table>
+                </div>
             </div>
 
-            <br></br>
 
-            <div className='planner-inputs'>
-                <div className='planner-form'>
-                    <div className='planner-tab-content'>
+            <div class="row">
+                <div class="col-12">
+                    <div class='grandparent'>
 
-                        <div className='planner-section-1'>                    
-                            <textarea id="story" name="story" rows="15" cols="35">
-                            Description...
-                            </textarea>
+                        <div class='plan-title'>Plan Created by:  user123</div>
+
+                        <div class='plan-nickname'>
+                            <div class="vote roundrect">
+                                <div class="increment up"></div>
+                                <div class="increment down"></div>
+                                <div class="count">Template for Likes : 105 Likes</div>
+                            </div>
                         </div>
+
                     </div>
                 </div>
-
-                <table className='planner-table'>
-                    <tr className='top-row'>
-                        <th>Times</th>
-                        <th>Day 1</th>
-                        <th>Day 2</th>
-                        <th>Day 3</th>
-                    </tr>
-                    <tr className='row1'>
-                        <td>9am</td>
-                        <td>Activity</td>
-                        <td>Activity</td>
-                        <td>Activity</td>
-
-                    </tr>
-                    <tr className='row2'>
-                        <td>12pm</td>
-                        <td>Activity</td>
-                        <td>Activity</td>
-                        <td>Activity</td>
-                    </tr>
-                    <tr className='row3'>
-                        <td>3pm</td>
-                        <td>Activity</td>
-                        <td>Activity</td>
-                        <td>Activity</td>
-                    </tr>
-                    <tr className='row4'>
-                        <td>7pm</td>
-                        <td>Activity</td>
-                        <td>Activity</td>
-                        <td>Activity</td>
-                    </tr>
-                    <tr className='row-5'>
-                        <td>11pm</td>
-                        <td>Activity</td>
-                        <td>Activity</td>
-                        <td>Activity</td>
-                    </tr>
-                </table>
             </div>
+    </div>
 
-            <br></br>
-
-            <div class='grandparent'>
-                <div class='plan-title'>Plan Created by:  user123</div>
-
-                <div class='plan-nickname'>
-                    <div class="vote roundrect">
-                        <div class="increment up"></div>
-                        <div class="increment down"></div>
-                        <div class="count">1053</div>
-                    </div>
-                </div>
-
-            </div>
-
-            
-
-        </div>
     )
 }
 
+// when user comes to page, they see list of plans, when they click the it populates - this will fetch plan
+// when edit button is clicked, you go to create plan 
 //  make component for pop up 
+// activity has unuqie id, the relationahip is that the base of the plan has the days array, which is day model which has day activity , activity must know whuch activity and at 9am the days 
+// is the master relationship
+// implement the back end to make the buttons work 
