a1200a8dbf48ea884174bd2e134092fd48550186
Tyler Flynn <tyler94flynn@gmail.com>
2022-07-26 14:19:09 -0400

    login/signup fix, account page placeholders added

--- client/src/pages/Account/Account.jsx
+++ client/src/pages/Account/Account.jsx
@@ -3,8 +3,32 @@ import "./Account.css";
 
 export default function Account() {
     return (
-        <div>
-            Account test
-        </div>
+        <body>
+            <div className="row">
+                <div className="col-1">
+                    <label>
+                        Profile picture (click to change)
+                    </label>
+                </div>
+                <div className="col-2">
+                    <label>
+                        Username
+                    </label>
+                    <br></br>
+                    <label>
+                        Bio
+                    </label>
+                </div>
+                <div className="col-3">
+                    <label>
+                        My Plans
+                    </label>
+                    <br></br>
+                    <label>
+                        __plans from backend__
+                    </label>
+                </div>
+            </div>
+        </body>
     )
 }
